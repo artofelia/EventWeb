2e3d100d9ebd86b8f97b734238e729b22b4ee168
Elia Portnoy <artofelia@gmail.com>
2014-11-10 11:43:20 -0500

    started front end and mongo base

---
+++ base.py
@@ -0,0 +1,78 @@
+import random
+from pymongo import Connection
+
+conn = Connection()
+
+db = conn['1247']
+
+
+def restart():
+    db.usertable.drop()
+    tdic = {'username': 'testing', 'password':'testing', 'intrests':['testing'],
+            'friends':['testing']}
+    db.usertable.insert(tdic)   
+
+def printData():
+    cres = db.usertable.find()
+    #{}, {'_id':False})
+    #print cres
+    #res = [r
+    for r in cres:
+        print r
+
+def validate(usernamei, passwordi):
+    cres = db.usertable.find({'username': usernamei,'password':passwordi})
+    res = [r for r in cres]  
+    if len(res)>0:
+        return True
+    return False
+
+def validateFriend(usernamei):
+    cres = db.usertable.find({'username': usernamei})
+    res = [r for r in cres]  
+    if len(res)>0:
+        return True
+    return False
+
+def addUser(usernamei, passwordi):
+    cres = db.usertable.find({'username':usernamei})
+    res = [r for r in cres]
+    print res
+    if len(res)>0:
+        return False
+    nu = {'username': usernamei, 'password':passwordi, 'intrests':[]
+          'friends':[]}
+    db.usertable.save(nu)
+    return True
+
+def updateUser(usernamei, passwordi, passwordn):
+    if validate (usernamei, passwordi):
+        db.usertable.update ({'username':usernamei, 'password':passwordi}, {"$set": {'username': usernamei, 'password':passwordn}})
+        return True
+    return False
+
+def updateIntest(usernamei, passwordi, intresti):
+    if validate(usernamei, passwordi):
+        db.usertable.update({'username':usernamei, 'password':passwordi},
+                            {$addToSet: {'intrests': intresti}})
+        return True
+    return False
+
+def updateFriend(usernamei, passwordi, friendi):
+    if validate(usernamei, passwordi):
+        if validateFriend(friendi):
+            db.usertable.update({'username':usernamei, 'password':passwordi},
+                            {$addToSet: {'friends': friendi}})
+            return True
+        return False
+    return False
+
+def getIntrest(usernamei):
+    if validateFriend(usernamei):
+        cres = db.usertable.find({'username': usernamei,'password':passwordi})
+        res = [r for r in cres]  
+        if len(res)>0:
+            return True
+        return False
+    return False
+    
