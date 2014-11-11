1ed48914a9c65e634f7fdd4d8b0fa94bc43b1211
abrarahmed56 <abrarahmed257@gmail.com>
2014-11-11 13:39:08 -0500

    retrieving api done, with preset searches

--- eventSearch.py
+++ eventSearch.py
@@ -3,21 +3,26 @@ import urllib2, json
 app_key = "FV8xFzGKxmWvqtgC"
 url="http://api.eventful.com/json/events/search?"
 
-queryWhat = "Dumb and Dumber To"
+queryWhat = "Dumb%20and%20Dumber%20To"
 queryWhen = "Today"
-queryWhere = "NYC"
+queryWhere = None#"NYC"
+andNeeded = True
 
 if queryWhat != None:
     url = url + "q=" + queryWhat
+#if andNeeded:
+#url = url + "q=" + queryWhat
+        
 if queryWhen != None:
     url = url + "&t=" + queryWhen
 if queryWhere != None:
     url = url + "&l=" + queryWhere
 
+url = url + "&app_key=" + app_key
 request = urllib2.urlopen(url)
 result = request.read()
 d = json.loads(result)
-#print d
-#rlist = d['responseData']['results']
+print d
+rlist = d['responseData']['results']
 #for r in rlist:
- #   print r
+    #print r
